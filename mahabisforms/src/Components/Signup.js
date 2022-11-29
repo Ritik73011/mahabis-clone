8a8cee5f642ad16c1e10267ea6a6cd8bc231c553
sonukryadav <sonukryadav56@gmail.com>
2022-11-29 22:16:23 +0530

    create account layout created

--- mahabisforms/src/Components/Signup.js
+++ mahabisforms/src/Components/Signup.js
@@ -1,7 +1,7 @@
-import React, { memo, useState } from 'react';
+import React, { memo, useState, useEffect } from 'react';
 import './signup.css';
 import TextField from '@mui/material/TextField';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import FilledInput from '@mui/material/FilledInput';
@@ -9,8 +9,13 @@ import InputAdornment from '@mui/material/InputAdornment';
 import IconButton from '@mui/material/IconButton';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
-import VisibilityIcon from '@mui/icons-material/Visibility';
-import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import LinearProgress from '@mui/material/LinearProgress';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { Divider } from '@mui/material';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
 
 
 
@@ -24,15 +29,10 @@ function Signup(){
         showPassword:'',
     });
 
-    // const [values, setValues] = useState({
-    //     password: '',
-    //     showPassword:'',
-    // });
-
-    const handleChange = (event) => {
-        // setSignup({ ...signup, [event.target.name]: event.target.value });
-    };
-    console.log(signup.password)
+    const [sigl, setSigl] = useState({
+        signal: '🔍',
+        color: 'inherit',
+    });
 
     const handleClickShowPassword = () => {
         setSignup({
@@ -46,9 +46,57 @@ function Signup(){
     };
 
     const onchange = (e) => {
-        setSignup(signup => ({ ...signup, [e.target.name]: e.target.value}));
+        setSignup(signup => ({ ...signup, [e.target.name]: e.target.value }));
     }
-    console.log(signup.showPassword);
+
+    useEffect(() => {
+        let passLength = signup.password.length;
+
+        if (passLength===0) {
+            setSigl({
+                signal: '🔍',
+                color: 'inherit',
+            })
+        }
+
+        if (passLength > 0 && passLength < 5) {
+            setSigl({
+                signal: 'Weak🛠️',
+                color: 'secondary',
+            })
+        }
+
+        if (passLength >=5 && passLength < 8) {
+            setSigl({
+                signal: 'Moderate👀',
+                color: 'secondary',
+            })
+        }
+
+        if (passLength >= 8) {
+            setSigl({
+                signal: 'Strong🔐',
+                color: 'success',
+            })
+        }
+    }, [signup]);
+
+
+    function LinearProgressWithLabel(props) {
+        return (
+            <Box sx={{ display: 'flex', alignItems: 'center', width:270 }}>
+                <Box sx={{ width: '100%', mr: 1, }}>
+                    <LinearProgress variant="indeterminate" sx={{ height: 8, borderRadius: 10, }} color={ sigl.color } />
+                </Box>
+                <Box sx={{ minWidth:170, fontSize:15 }}>
+                    <Typography variant="body5" color="text.secondary">{props.value}</Typography>
+                </Box>
+            </Box>
+        );
+    }
+
+
+    // console.log(signup);
 
     return (
         <div>
@@ -75,7 +123,7 @@ function Signup(){
                     </div>
 
                     <div className='password'>
-                        <FormControl sx={{ mt: 2, width: true }} variant="filled">
+                        <FormControl sx={{ mt: 3, width: true }} variant="filled">
                             <InputLabel htmlFor="filled-adornment-password">Password</InputLabel>
                             <FilledInput
                                 id="filled-adornment-password"
@@ -90,8 +138,7 @@ function Signup(){
                                             onMouseDown={handleMouseDownPassword}
                                             edge="end"
                                         >
-                                            {/* {signup.showPassword ? <VisibilityOff /> : <Visibility />} */}
-                                            {signup.showPassword ? <p>Hide</p> : <p>show</p>}
+                                        {signup.showPassword ? <VisibilityOff /> : <Visibility />}
                                         </IconButton>
                                     </InputAdornment>
                                 }
@@ -99,12 +146,51 @@ function Signup(){
                         </FormControl>
                     </div>
 
+                    <div className='passwordSignal'>
+                        <Box sx={{ width: '30%' }}>
+                            <LinearProgressWithLabel color='success' value={sigl.signal} />
+                        </Box>
+                    </div>
+
+                    <div className='TandC'>
+                        <p>By Signing up, you agree to our <NavLink to='#' id='TandCi'>Terms of Service</NavLink> and <NavLink to='#' id='TandCi'>Privacy Policy </NavLink></p>
+                    </div>
+
+                    <div className='createAccount'>
+                        <button class="custom-btn btn-12"><span ><NavLink to='#'id='createAccounti'>Click!</NavLink></span><span id='createAccounti'>Create Account</span></button>
+                    </div>
+
+                    <div className='divider1'>
+                        <Divider sx={{color:'black', fontSize:12}} >Sign up with</Divider>
+                    </div>
+
+
+                    <div className='logbtns'>
+
+                        <Stack direction="row" spacing={2} >
+                            <NavLink to='#' id='acct1'>
+                                <Button variant="outlined" sx={{width:'120px'} }>
+                                    <img src="https://www.clipartmax.com/png/middle/105-1055517_google-chrome-icon-google-logo-round-png.png" alt='img' width='32' />Google
+                                </Button>
+                            </NavLink>
+
+                            <NavLink to='#' id='acct1'>
+                                <Button variant="outlined" sx={{ width: '120px' }}>
+                                    <img src="https://image.similarpng.com/very-thumbnail/2021/05/Facebook-logo-design-on-transparent-background-PNG.png" alt='img' width='30' />Facebook
+                                </Button>
+                            </NavLink>
+
+                            <NavLink to='#' id='acct1'>
+                                <Button variant="outlined" sx={{ width: '120px' }}>
+                                    <img src="https://e7.pngegg.com/pngimages/708/311/png-clipart-twitter-twitter-thumbnail.png" alt='img' width='30' />Twitter
+                                </Button>
+                            </NavLink>
+                        </Stack>
+
+                    </div>
+
                 </div>
-
             </div>
-
-
-
         </div>
     )
 }
