042fe060030b1b530907a9715783c5de97cbcf40
Ritik73011 <ritikkumarsingh730@gmail.com>
2022-12-01 20:49:08 +0530

    drawer added in cart icon

--- landing_page/src/Components/Navbar/Icons/Icon.jsx
+++ landing_page/src/Components/Navbar/Icons/Icon.jsx
@@ -1,15 +1,29 @@
 
-import { Flex } from '@chakra-ui/react';
+import { Divider, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerHeader, DrawerOverlay, Flex, useDisclosure } from '@chakra-ui/react';
 import './Icon.css';
 function Icon() {
+  const { isOpen, onOpen, onClose } = useDisclosure()
   return (
     <div className='iconDiv'>
      <img className='acountInfo' src="https://cdn.shopify.com/s/files/1/0238/5795/t/29/assets/icon-myaccount.png?v=85297090699021414941656596220" alt="" />
         <img src="https://gepi.global-e.com/content/images/flags/in.png" alt="" />
-        <Flex>
+        <Flex onClick={()=>{onOpen()}}>
         <img src="https://cdn.shopify.com/s/files/1/0238/5795/t/29/assets/icon-cart.png?v=182661896060636914941656596211" alt="" />
         <span className='cartQnt'>99</span>
         </Flex>
+
+
+        <Drawer onClose={onClose} placement='right' isOpen={isOpen} size={'sm'}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>cart</DrawerHeader>
+          <Divider/>
+          <DrawerBody>
+            Add Cart Component
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
     </div>
   );
 }
